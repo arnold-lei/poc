17cf05f7941086e2ca2003dcea2628d8da72060e
Arnold Lei <arnold.lei88@gmail.com>
2016-06-13 10:38:22 -0400

    first commit

---
+++ assets/js/mapster.js
@@ -0,0 +1,20 @@
+(function(window, google){
+
+  var Mapster = (function(){
+    function Mapster(element, opts){
+      this.gMap = new google.maps.Map(element, opts);
+    }
+    Mapster.prototype = {
+
+    };
+    return Mapster;
+  }());
+
+  Mapster.create = function(element, opts) {
+    return new Mapster(element, opts);
+  };
+  
+  window.Mapster = Mapster;
+
+}(window, google));
+
---
+++ assets/js/poc.js
@@ -0,0 +1,55 @@
+// $('document').ready(function({
+(function(window, google, mapster){
+  //firebase connection 
+  var firebase = new Firebase("https://arnoldleitestrcb.firebaseio.com/");
+
+  //map options
+  var options = mapster.MAP_DEFAULT_OPTIONS;
+  //Setting the map to the div 
+  element = document.getElementById('map');
+  //creating the map object
+  var map = mapster.create(element, options);
+
+
+  //creates markers on click
+  map.addListener('click', function(e) {
+    var marker = new google.maps.Marker({
+      position: {lat: e.latLng.lat(), lng: e.latLng.lng()},
+      map: map
+    });
+    firebase.push({lat: e.latLng.lat(), lng: e.latLng.lng()});
+  });
+
+  //firebase code for when the user adds something to firebase
+  firebase.on("child_added", function(snapshot, prevChildKey) {
+  // Get latitude and longitude from the cloud.
+    var newPosition = snapshot.val();
+
+    // Create a google.maps.LatLng object for the position of the marker.
+    // A LatLng object literal (as above) could be used, but the heatmap
+    // in the next step requires a google.maps.LatLng object.
+    var latLng = new google.maps.LatLng(newPosition.lat, newPosition.lng);
+
+    // Place a marker at that location.
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  });
+
+}(window, google, window.Mapster))
+
+
+
+// function initMap() {
+//   var map = new google.maps.Map(document.getElementById('map'), );
+
+
+
+// }
+
+
+
+
+
+// });)
